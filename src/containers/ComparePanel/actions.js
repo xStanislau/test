3a571e6445c864b4b483ab1cc3d1d87a20d44b13
Stanislau <grench333@gmail.com>
2018-12-21 15:59:03 +0300

    Update actions.js

--- src/containers/ComparePanel/actions.js
+++ src/containers/ComparePanel/actions.js
@@ -1,10 +1,11 @@
-import {ROTATE_PRODUCT, SWITCH_UNIT_SYSTEM} from '../../constants/actions';
+// import { ROTATE_PRODUCT } from '../../constants/actions';
+import { SWITCH_UNIT_SYSTEM } from '../../constants/actions';
 
-export function rotateProduct(product) {
-    return {
-        type: ROTATE_PRODUCT
-    };
-}
+// export function rotateProduct(product) {
+//     return {
+//         type: ROTATE_PRODUCT
+//     };
+// }
 
 export function switchUnitSystem(payload) {
     return {
